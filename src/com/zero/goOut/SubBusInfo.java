2d1c24291b9323748d14e4337cd5fecaf902c7d1
king <king@king-PC>
2014-01-05 14:49:29 +0800

    busInfo 中抽出来的封装类

---
+++ src/com/zero/goOut/SubBusInfo.java
@@ -0,0 +1,35 @@
+package com.zero.goOut;
+
+public class SubBusInfo {
+	private String station;
+	private int number;
+	private boolean arrived;
+	private int distance;
+	
+	public String getStation() {
+		return station;
+	}
+	public void setStation(String station) {
+		this.station = station;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public boolean isArrived() {
+		return arrived;
+	}
+	public void setArrived(boolean arrived) {
+		this.arrived = arrived;
+	}
+	public int getDistance() {
+		return distance;
+	}
+	public void setDistance(int distance) {
+		this.distance = distance;
+	}
+	
+	
+}
